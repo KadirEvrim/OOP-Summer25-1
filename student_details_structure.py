099c65783097a7c735891ee980875ccebc7626a8
KadirEvrim <kadir1907756@gmail.com>
2025-03-13 11:12:45 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,1 +1,14 @@
-a
+first_name = "Kadir" 
+last_name = "Evrim"
+index_number "35096"
+nationality = "Turkish" 
+starting_date = "3/13/2025" 
+courses = ["Computer science"]
+
+
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Index Number: {index_number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses: {', '.join(courses)}")
